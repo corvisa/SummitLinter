53e8ca8422cddb08d24f4ebf74ce011f7bb63602
Anderson, Chase <Chase.Anderson@corvisa.com>
2014-06-18 10:47:21 -0500

    initial commit

---
+++ linter.py
@@ -0,0 +1,55 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+#
+# Copyright (c) 2014 CorvisaCloud, LLC
+#
+# License: MIT
+#
+
+"""This module exports the LuaSummit plugin class."""
+
+from SublimeLinter.lint import Linter, util
+
+
+class SummitLua(Linter):
+    """Provides an interface to luacheck for the SummitEditor syntax."""
+    syntax = 'summit'
+    tempfile_suffix = 'lua'
+    defaults = {
+        '--ignore:,': '',
+        '--only:,': '',
+        '--limit=': None,
+        '--globals: ': 'channel,require',
+    }
+    comment_re = r'\s*-[-*]'
+    inline_settings = ('ignore', 'limit', 'only', 'globals')
+    cmd = 'luacheck @ *'
+    regex = r'^(?P<filename>[^:]+):(?P<line>\d+):(?P<col>\d+): (?P<message>.*)$'
+    def build_args(self, settings):
+        """Return args, transforming --ignore, --only, and --globals args into a format luacheck understands."""
+        args = super().build_args(settings)
+        print(args)
+        try:
+            index = args.index('--ignore')
+            # Split the comma-separated arg after --ignore into separate elements
+            vars = args[index + 1].split(',')
+            args[index + 1:index + 2] = vars
+        except ValueError:
+            pass
+        
+        try:
+            index = args.index('--only')
+            vars = args[index + 1].split(',')
+            args[index + 1:index + 2] = vars
+        except ValueError:
+            pass
+
+        try:
+            index = args.index('--globals')
+            vars = args[index + 1].split(',')
+            args[index + 1:index + 2] = vars
+        except ValueError:
+            pass
+        return args
